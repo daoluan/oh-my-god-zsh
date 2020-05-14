492e0e2cc97ef353595672ea9fbc6b7f085eccd7
dylanzheng <g.daoluan@gmail.com>
2020-05-14 12:20:25 +0800

    style: screen log
    Change-Id: I7f3c642b80aca2f8408442b5e94c4af996ecfbea

--- env.sh
+++ env.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+Green='\033[0;32m'
+Color_Off='\033[0m'
+
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 for ef in $(find $DIR/*/ -name 'env.sh');
 do
-    echo "source $ef"
+    echo "${Green}sourcing $ef${Color_Off}"
     source $ef
 done
