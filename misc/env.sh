2fdc45ae13f4b6453db96ada0979710afffe8edc
dylanzheng <g.daoluan@gmail.com>
2020-05-27 23:08:25 +0800

    feat: supervisor cmd

--- misc/env.sh
+++ misc/env.sh
@@ -54,3 +54,45 @@ alias stsu="supervisorctl status"
 
 # other
 export LD_LIBRARY_PATH="/usr/local/lib:$LD_LIBRARY_PATH"
+
+plugins=(
+  git
+  extract
+)
+
+source $ZSH/oh-my-zsh.sh
+
+# supervisor
+if [[ "$(uname)" == "Linux" ]]; then
+    resu () {
+        Color_Off='\033[0m'       # Text Reset
+        Green='\033[0;32m'        # Green
+
+        processes=$(supervisorctl status|awk '{print $1}'| tr '\n' ' ')
+        echo -e ${Green}'which one you want to restart'${Color_Off}
+        proarr=(`echo ${processes} | sed 's/ /\n/g'`)
+
+        let i=0
+        for i in $(seq 0 ${#proarr[@]})
+        do
+            if [[ -z "${proarr[$i]// }"  ]]; then
+                continue
+            fi
+
+            if [ $# -eq 1 ]; then
+                if echo "${proarr[$i]}" | grep "$1" -q > /dev/null; then
+                    echo $i ${proarr[$i]}
+                fi
+            else
+                echo $i ${proarr[$i]}
+            fi
+            let i++
+        done
+
+        read idx
+
+        printf "staring \e[93m${proarr[$idx]}\033[0m\n"
+        echo ${proarr[$i]}
+        supervisorctl restart "${proarr[$idx]}"
+    }
+fi
