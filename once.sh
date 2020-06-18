77b421403d77d8ee7410b986f2972d713c833b97
dylanheng <g.daoluan@gmail.com>
2020-06-18 23:21:16 +0800

    fix: autojump install fixed

--- once.sh
+++ once.sh
@@ -4,7 +4,7 @@
 # pip3 in default
 pip3 config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
 
-[ -d ~/.autojump ] || git clone https://github.com/wting/autojump.git ~/.autojump
+[ -d ~/.autojump ] || $(git clone https://github.com/wting/autojump.git /tmp/autojump && cd /tmp/autojump/ && ./install.py && cd -)
 
 [ -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions ] || git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 [ -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting ] || git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
