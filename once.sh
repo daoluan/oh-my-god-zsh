c84d502ff250e1630c1f45d0d08bdceef17c54fe
dylanzheng <g.daoluan@gmail.com>
2023-05-17 15:44:30 +0800

    fix: once.sh

--- once.sh
+++ once.sh
@@ -1,10 +1,17 @@
 #! /bin/bash
 
-# pip
-# pip3 in default
-pip3 config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
 
-[ -d ~/.autojump ] || $(git clone https://github.com/wting/autojump.git /tmp/autojump && cd /tmp/autojump/ && ./install.py && cd -)
+if [ -d ~/.autojump ]; then
+    echo "autojump Installed"
+else
+    rm -fr /tmp/autojump
+    git clone https://github.com/wting/autojump.git /tmp/autojump
+    cd /tmp/autojump/
+    ./install.py
+    cd -
+fi
+
+
 
 [ -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions ] || git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 [ -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting ] || git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
