abe73a75697319085a71ed1de5ee9502a97b5e25
dylanzheng <dylanzheng@tencent.com>
2025-09-15 15:09:24 +0800

    fix once.sh

--- once.sh
+++ once.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 
-
 if [ -d ~/.autojump ]; then
     echo "autojump Installed"
 else
@@ -11,15 +10,15 @@ else
     cd -
 fi
 
-
-
 [ -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions ] || git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 [ -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting ] || git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 cp rc/.tigrc ~/
 
-# vscode/cursor repeate keypress
-defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+if [[ "$(uname)" == "Darwin" ]]; then
+    # vscode/cursor repeate keypress
+    defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+fi
 
 mkdir -p ~/.pip
 cp rc/pip.conf ~/.pip/pip.conf
